771c092ccb3240150278190ce7e05ada9838820a
shisiying <751401459@qq.com>
2017-07-03 17:20:07 +0800

    更改邮件密码

--- tc_zufang/tc_zufang/utils/message.py
+++ tc_zufang/tc_zufang/utils/message.py
@@ -4,7 +4,7 @@ from email.mime.text import MIMEText
 from email.header import Header
 def sendMessage_warning():
     server = smtplib.SMTP('smtp.163.com', 25)
-    server.login('seven_2016@163.com', 'ssy102009')
+    server.login('seven_2016@163.com', '*******')
     msg = MIMEText('爬虫Master被封警告！请求解封！', 'plain', 'utf-8')
     msg['From'] = 'seven_2016@163.com <seven_2016@163.com>'
     msg['Subject'] = Header(u'爬虫被封禁警告！', 'utf8').encode()
